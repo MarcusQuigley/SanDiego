92d1dee7ad7c2fc37d415dab0cb25d49bb578431
Marcus <marcusquigley@gmail.com>
2020-02-13 20:01:51 -0500

    adding fib

---
+++ Chap1. AlgorithmicToolbox/Week2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chap1.AlgorithmicToolbox
+{
+    class Week2<T> : ILesson
+    {
+
+        public int Fibonacci(int n)
+        {
+            int n0 = 0;
+            int n1 = 1;
+            int result=0;
+            if (n == 0) return n0;
+            if (n == 1) return n1;
+            for (int i = 2; i < n; i++)
+            {
+                result = n0 + n1;
+                n0 = n1;
+                n1 = result;
+            }
+            return result;
+        }
+
+        public bool StressTest(int maxArraySize, int maxValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
