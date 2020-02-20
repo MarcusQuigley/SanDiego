be47caf0626b37bdc4c9a88cb4b100536e1c1d7a
marcu <marcu@DESKTOP-165KC1R>
2020-02-20 15:58:14 -0500

    MoneyChange

---
+++ Tests/Exercises/Week3ExercisesTests.cs
@@ -0,0 +1,25 @@
+﻿using Chap1.AlgorithmicToolbox.Exercises;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace Chap1.AlgorithmicToolbox.Tests.Exercises
+{
+    public class Week3ExercisesTests
+    {
+        Week3Exercises sut;
+        public Week3ExercisesTests()
+        {
+            sut = new Week3Exercises();
+        }
+
+        [Theory]
+        [InlineData(2,2)]
+        [InlineData(28, 6)]
+        public void CheckGetChange(int weight, int expected)
+        {
+            var actual = sut.GetChange(weight);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
