228fcae1160172b295851d669ab2258d4d54eb21
marcu <marcu@DESKTOP-158DNT8>
2020-02-27 11:55:44 -0500

    wip

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ConsoleApp2
@@ -12,32 +13,101 @@ namespace ConsoleApp2
             //                       .Split(' ')
             //                       .Select(i => int.Parse(i))
             //                       .ToArray();
+ 
+            
+
             var n = int.Parse(Console.ReadLine());
-            var profits = Console.ReadLine()
-                                     .Split(' ')
-                                     .Select(i => int.Parse(i))
-                                     .ToArray();
-            var clicks = Console.ReadLine()
-                                     .Split(' ')
-                                     .Select(i => int.Parse(i))
-                                     .ToArray();
-
-            Console.WriteLine(MaxAdRevenue(profits, clicks));
-        }
-
-        static   long MaxAdRevenue(int[] profits, int[] clicks)
-        {
-            long result = 0;
-            Array.Sort(profits);
-            Array.Sort(clicks);
-            for (int i = 0; i < profits.Length; i++)
+            Segment[] segments = new Segment[n];
+            for (int i = 0; i < n; i++)
             {
-                var profit = profits[i] * (long)clicks[i];
-                result += profit;
+                var segment = Console.ReadLine()
+                                     .Split(' ')
+                                     .Select(i => int.Parse(i))
+                                     .ToArray();
+                segments[i] = new Segment(segment[0], segment[1]);
             }
-
-            return result;
+            int[] points = CollectingSignatures(segments);
+            Console.WriteLine("----------");
+            Console.WriteLine(points.Length);
+            foreach (var point in points)
+            {
+                Console.Write(point + " ");
+            }
         }
 
+        static int[] CollectingSignatures(Segment[] segments)
+        {
+            if (segments == null || segments.Length == 0)
+                return new int[] { 0 };
+            if (segments.Length == 1)
+                return new int[] { segments[0].End };
+
+            int index = 0;
+            int startIndex = 0;
+            int n = segments.Length;
+            List<int> points = new List<int>();
+            Array.Sort(segments, new SegmentComparer());
+
+            while (index <= n)
+            {
+                startIndex = index;
+                int startPoint = segments[startIndex].End;
+                while (index < n && segments[index].ContainsPoint(startPoint))// segments[index] != segments[startIndex]) //need to see if startIndex segment touchs all new segments
+                {
+                    index += 1;
+                    //need to get the highest value thats touching the segments
+                }
+                //  if (index > n) throw new Exception();
+                if (index <= n)
+                {
+                    points.Add(startPoint); //need to add this highest value
+                                            //      index += 1;
+                    if (index == n)
+                        break;
+                }
+            }
+            return points.ToArray();
+        }
+
+    }
+    public class Segment
+    {
+        public int Start { get; }
+        public int End { get; }
+
+        public Segment(int start, int end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+
+        public bool ContainsSegment(Segment segment)
+        {
+            if (this.Start >= segment.Start && this.End >= segment.End)
+                return true;
+            return false;
+        }
+
+        public bool ContainsPoint(int point)
+        {
+            return (point - Start) * (End - point) >= 0;
+        }
+
+        public override string ToString()
+        {
+            return $"S:{Start} E:{End}";
+        }
+    }
+
+    public class SegmentComparer : IComparer<Segment>
+    {
+        public int Compare(Segment x, Segment y)
+        {
+            if (x.Start < y.Start)
+                return -1;
+            else if (x.Start == y.Start)
+                return (x.End <= y.End) ? -1 : 1;
+            return 1;
+        }
     }
 }
