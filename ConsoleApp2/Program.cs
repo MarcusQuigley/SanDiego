fc82c872f1f3fa21465add949f614597c570f737
marcu <marcu@DESKTOP-158DNT8>
2020-03-03 09:44:36 -0500

    prob 6. Max prizes

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -13,103 +13,35 @@ namespace ConsoleApp2
             //                       .Split(' ')
             //                       .Select(i => int.Parse(i))
             //                       .ToArray();
- 
-            
+
+
 
             var n = int.Parse(Console.ReadLine());
-            Segment[] segments = new Segment[n];
-            for (int i = 0; i < n; i++)
+             
+            var  answer = MaxPrizes(n);
+            Console.WriteLine(answer.Length);
+            foreach (var a in answer)
             {
-                var segment = Console.ReadLine()
-                                     .Split(' ')
-                                     .Select(j => int.Parse(j))
-                                     .ToArray();
-                segments[i] = new Segment(segment[0], segment[1]);
-            }
-            int[] points = CollectingSignatures(segments);
-            Console.WriteLine(points.Length);
-            foreach (var point in points)
-            {
-                Console.Write(point + " ");
+                Console.Write($"{a} " );
             }
         }
 
-        static int[] CollectingSignatures(Segment[] segments)
+        static long[] MaxPrizes(int n)
         {
-            if (segments == null || segments.Length == 0)
-                return new int[] { 0 };
-            if (segments.Length == 1)
-                return new int[] { segments[0].End };
-
-            int index = 0;
-            int startIndex = 0;
-            int n = segments.Length;
-            List<int> points = new List<int>();
-            Array.Sort(segments, new SegmentLeftComparer());
-            int lowestEndPoint = -1;
-            while (index < n)
+            int current = 0;
+            List<long> results = new List<long>();
+            while (n > 0)
             {
-                startIndex = index;
-                var set1 = segments[startIndex];
-                var nextIndex = startIndex + 1;
-                lowestEndPoint = set1.End;
-
-                while (nextIndex < n)
+                current += 1;
+                if (current * 2 >= n)
                 {
-                    var set2 = segments[nextIndex];
-                    if (Intersects(lowestEndPoint, set2.Start))
-                    {
-                        if (set2.End < lowestEndPoint)
-                        {
-                            lowestEndPoint = set2.End;
-                        }
-                        nextIndex += 1;
-                    }
-                    else
-                    {
-                        points.Add(lowestEndPoint);
-
-                        break;
-                    }
-                }
-                if (n == nextIndex)
-                {
-                    points.Add(lowestEndPoint);
+                    results.Add(n);
                     break;
                 }
-                index = nextIndex;
+                n -= current;
+                results.Add(current);
             }
-            return points.ToArray();
-        }
-
-        static bool Intersects(int end, int segmentStart)
-        {
-            return segmentStart <= end;
-        }
-
-    }
-    public class Segment
-    {
-        public int Start { get; }
-        public int End { get; }
-
-        public Segment(int start, int end)
-        {
-            this.Start = start;
-            this.End = end;
-        }
-
-        public override string ToString()
-        {
-            return $"S:{Start} E:{End}";
-        }
-    }
-
-    public class SegmentLeftComparer : IComparer<Segment>
-    {
-        public int Compare(Segment x, Segment y)
-        {
-            return x.Start - y.Start;
+            return results.ToArray();
         }
     }
 }
